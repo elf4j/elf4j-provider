f15c93ad8392f38c140afe4dc64565bf93feaa98
q3769 <wqt.net@gmail.com>
2023-03-08 21:08:49 -0600

    init

--- src/test/java/elf4j/impl/IntegrationTest.java
+++ src/test/java/elf4j/impl/IntegrationTest.java
@@ -4,16 +4,20 @@ import elf4j.Logger;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class IntegrationTest {
     @Nested
     class defaultLogger {
         @Test
         void hey() {
             Logger logger = Logger.instance();
-            logger.atInfo().log("Hello, world!");
 
+            logger.atInfo().log("Hello, world!");
             Exception issue = new Exception("Test ex message");
             logger.atWarn().log(issue, "Testing issue '{}' in {}", issue, this.getClass());
+
+            assertEquals(this.getClass().getName(), ((NativeLogger)logger).getName());
         }
     }
 }
